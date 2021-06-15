5ded4645186c537853eb1006ef28c4a5ddbbb231
vatsacr <vatsacr.93@gmail.com>
2021-06-15 12:21:00 -0500

    Using defaultProps to set default prop values

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
-const Header = ({ title }) => {
+const Header = ({ title, description }) => {
   return (
     <header>
       <h1>{title}</h1>
+      <p>{description}</p>
     </header>
   );
+};
+
+Header.defaultProps = {
+  title: "Task Tracker",
+  description:
+    "This is a task tracker mini-application that extensively uses the latest React features",
 };
 
 export default Header;
